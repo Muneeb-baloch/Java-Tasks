80b47771702c0b09c256e9e6dc799887887ee3e8
Muneeb-baloch <muneebmultan1999@gmail.com>
2025-08-25 15:56:40 +0500

    mplement a number guessing game where the program selects a random number and the user gets limited attempts. Less tries means more scores. Initially set score per attempt and limit. Example: Limit  10 and limitScore  10, means max score  100 user tried once and it was correct, score should be 100 user failed on first attempt, tried second and it was correct, score should be 90 and so on… At last attempt the game should end with game over and 0 scores

---
+++ src/NumberGuessingGame.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Guess the Number between 20");
+        int randomNumber = r.nextInt(20);
+        System.out.println(randomNumber);
+
+
+        int i ;
+        int maxscore = 10;
+
+        for (i = 1; i<= 10; i++){
+            System.out.println("Enter your number");
+            int userguess = sc.nextInt();
+            if (randomNumber == userguess){
+                int userscore = maxscore-i;
+                int x = userscore*10;
+                System.out.println("Your Guess is Right");
+                System.out.println("Your Score is : " + x);
+
+
+            }
+
+
+        }
+
+        System.out.println("You're out of tries");
+
+
+    }
+
+
+}
